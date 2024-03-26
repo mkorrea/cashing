c614cc3e0fedc84ac6e9a0fdc523aa03c921e2cd
mkorrea <mathscorrea@gmail.com>
2024-03-26 01:06:42 -0300

    add header and side menu

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
+import './style.css'
+import Header from './components/Header'
+import Menu from './components/Menu'
+import Main from './components/Main'
 
 function App() {
   return (
     <div>
-
+      <Header />
+      <Menu />
+      <Main />
     </div>
   );
 }
---
+++ src/components/Menu/index.js
@@ -0,0 +1,12 @@
+
+
+function Menu() {
+    return(
+    <div className="menu">
+        <a className="button">⌂</a>
+        <a className="button">🕮</a>
+    </div>
+    )
+}
+
+export default Menu;
