c5ac776629dee0536ad35d398d4781afa0d34aa5
mkorrea <mathscorrea@gmail.com>
2024-05-24 02:26:47 -0300

    set darkmode to register and login page

--- src/contexts/ThemeContexts.js
+++ src/contexts/ThemeContexts.js
@@ -1,15 +1,31 @@
-import React, { createContext, useState, useContext } from 'react'
+import React, { createContext, useState, useContext, useEffect } from 'react';
 
 const ThemeContext = createContext();
-export const useTheme = () => useContext(ThemeContext)
+export const useTheme = () => useContext(ThemeContext);
 
 export const ThemeProvider = ({ children }) => {
-   const [ darkMode, setDarkMode ] = useState(false)
+   const [darkMode, setDarkMode] = useState(false);
+
+   useEffect(() => {
+      const savedTheme = localStorage.getItem('darkMode');
+      if (savedTheme) {
+         setDarkMode(savedTheme === 'true');
+         applyTheme(savedTheme === 'true');
+      }
+   }, []);
 
    const toggleDarkMode = () => {
-      setDarkMode( prevMode => !prevMode )
-      if(!darkMode) {
-         //   modo escuro
+      setDarkMode(prevMode => {
+         const newMode = !prevMode;
+         applyTheme(newMode);
+         localStorage.setItem('darkMode', newMode);
+         return newMode;
+      });
+   };
+
+   const applyTheme = (isDarkMode) => {
+      if (isDarkMode) {
+         // modo escuro
          document.documentElement.style.setProperty('--background-primary-color', '#121214');
          document.documentElement.style.setProperty('--background-secondary-color', '#1d2125');
          document.documentElement.style.setProperty('--color-main', '#121214');
@@ -19,7 +35,7 @@ export const ThemeProvider = ({ children }) => {
          document.documentElement.style.setProperty('--main-text-color', '#fff');
          document.documentElement.style.setProperty('--background-tabela', 'rgba(26, 28, 31, 0.9)');
       } else {
-         //   modo claro
+         // modo claro
          document.documentElement.style.setProperty('--background-primary-color', '#fff');
          document.documentElement.style.setProperty('--background-secondary-color', '#e1e8eb84');
          document.documentElement.style.setProperty('--color-main', '#0cb3b8');
@@ -28,12 +44,12 @@ export const ThemeProvider = ({ children }) => {
          document.documentElement.style.setProperty('--text-color', '#666666');
          document.documentElement.style.setProperty('--main-text-color', '#000');
          document.documentElement.style.setProperty('--background-tabela', 'rgba(238, 243, 243, 0.9)');
-         }
       }
+   };
 
    return (
-      <ThemeContext.Provider value={ {darkMode, toggleDarkMode }} >
+      <ThemeContext.Provider value={{ darkMode, toggleDarkMode }}>
          {children}
       </ThemeContext.Provider>
-   )
-}
+   );
+};
