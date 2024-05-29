e8ddf11b4e040e40613c2428e41727fc0932d39d
mkorrea <mathscorrea@gmail.com>
2024-05-28 22:49:46 -0300

    att firebase connection

--- src/firebaseConnections.js
+++ src/firebaseConnections.js
@@ -13,15 +13,26 @@ import { getFirestore } from 'firebase/firestore';
 //    measurementId: "G-PLS242LHPQ"
 //  };
 
+// const firebaseConfig = {
+//    apiKey: "AIzaSyBxxeQLZrcLBOup_3PFXCJBuQlc7s9AxLA",
+//    authDomain: "projeto-cashing.firebaseapp.com",
+//    projectId: "projeto-cashing",
+//    storageBucket: "projeto-cashing.appspot.com",
+//    messagingSenderId: "358535484148",
+//    appId: "1:358535484148:web:bc978dc726905118b444a1",
+//    measurementId: "G-KGTQC6SLYV"
+//  };
+
 const firebaseConfig = {
-   apiKey: "AIzaSyBxxeQLZrcLBOup_3PFXCJBuQlc7s9AxLA",
-   authDomain: "projeto-cashing.firebaseapp.com",
-   projectId: "projeto-cashing",
-   storageBucket: "projeto-cashing.appspot.com",
-   messagingSenderId: "358535484148",
-   appId: "1:358535484148:web:bc978dc726905118b444a1",
-   measurementId: "G-KGTQC6SLYV"
+   apiKey: "AIzaSyBnV4uXw_w-6Vg1h_TiSdJQVpIPgPIOV70",
+   authDomain: "teste-453eb.firebaseapp.com",
+   projectId: "teste-453eb",
+   storageBucket: "teste-453eb.appspot.com",
+   messagingSenderId: "286683843713",
+   appId: "1:286683843713:web:7895196f7c600b80ff9180",
+   measurementId: "G-WS0Z3Q4135"
  };
+ 
 
  const firebaseApp = initializeApp(firebaseConfig);
  const db = getFirestore(firebaseApp);
